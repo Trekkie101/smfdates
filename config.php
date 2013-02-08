1098cfc9939aa21e6c1fb649edf6eaa666d6c442
Graeme Spence <trekkie101@gmail.com>
2013-02-08 09:14:25 -0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Config
+// Public Domain Code 
+// Use for anything :)
+
+// Database Configuration
+$dbuser = 'username';
+$dbpass = 'password';
+$dbhost = 'localhost';
+$dbname = 'user_smfdates';
+
+?>
